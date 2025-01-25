57fe5b9124bfaca87ca0befd530aa045f53b91ef
Victor <147546288+victorabiezer@users.noreply.github.com>
2025-01-25 14:05:23 -0600

    Create sum_of_two_integers.cpp

---
+++ Basic/sum_of_two_integers.cpp
@@ -0,0 +1,38 @@
+/* 
+
+Problem: Sum of Two Integers
+Link: Add a LeetCode link here if available
+Description: Given two integers num1 and num2, return their sum.
+
+
+class Solution 
+{
+public:
+    int sum(int num1, int num2) 
+    {
+      __________
+    }
+};
+
+
+Example 1:
+Input: num1 = 12, num2 = 5
+Output: 17
+
+Example 2:
+Input: num1 = -10, num2 = 4
+Output: -6
+
+CONSTRAINTS: -100 <= num1, num2 <= 100
+
+*/
+
+class Solution 
+{
+public:
+    int sum(int num1, int num2) 
+    {
+      return num1 + num2;
+    }
+};
+  
